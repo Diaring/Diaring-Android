e043eccc24908b140d414abf455973b38852f2d3
Jaesung Lee <viki0114@naver.com>
2022-04-25 17:05:02 +0900

    [ADD] #1 BaseActivity, BaseFragment 추가

---
+++ Diaring/app/src/main/java/com/oss/diaring/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.oss.diaring.ui.main
+
+import android.os.Bundle
+import com.oss.diaring.R
+import com.oss.diaring.databinding.ActivityMainBinding
+import com.oss.diaring.ui.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
