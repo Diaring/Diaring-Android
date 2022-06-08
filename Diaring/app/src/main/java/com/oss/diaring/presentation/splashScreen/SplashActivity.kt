946d811648fc61b9a25f00135f8fda716f962b02
Soulddgy <91243236+Soulddgy@users.noreply.github.com>
2022-06-08 23:27:14 +0900

    [FEAT] #20 스플래시 레이아웃 구현 [FEAT] #16 로그인 액티비티 구현

--- Diaring/app/src/main/java/com/oss/diaring/presentation/splashScreen/SplashActivity.kt
+++ Diaring/app/src/main/java/com/oss/diaring/presentation/splashScreen/SplashActivity.kt
@@ -3,14 +3,14 @@ package com.oss.diaring.presentation.splashScreen
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.oss.diaring.presentation.Onboarding.OnboardingActivity
+import com.oss.diaring.presentation.login.LoginActivity
 import com.oss.diaring.presentation.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, OnboardingActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
